ccae54faf5ae2c1ce14599b6e7677148a992352e
shavan810 <creative.shavan@gmail.com>
2025-08-18 02:36:26 +0530

    second commit

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -26,7 +26,7 @@ export default function Navbar() {
 
         {/* Mobile Hamburger Button (Center) */}
         <button
-          className="md:hidden text-2xl w-[180px] focus:outline-none absolute left-1/2 transform -translate-x-1/2"
+          className="md:hidden text-2xl w-[150px] focus:outline-none absolute left-1/2 transform -translate-x-1/2"
           onClick={() => setOpen(!open)}
         >
           {open ? <FaTimes /> : <FaBars />}
